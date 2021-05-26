00c43fc69e7a83f5f5a033123b75484fd395fcc0
Vagrant User <vagrant@multicloud>
2021-05-26 19:57:56 -0300

    Convertendo Credential em Variável

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/lab-devops-cloud-314819-71ba6aedd9d8.json")
-
   project = "lab-devops-cloud-314819"
   region  = "us-central1"
   zone    = "us-central1-c"
