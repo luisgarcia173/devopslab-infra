0e3fc02d9acd15b17e10f57bb5b57ba8c8d5de4c
Vagrant User <vagrant@multicloud>
2021-05-26 21:39:24 -0300

    Tentando publicar App

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,5 +1,10 @@
-resource "google_app_engine_application" "app" {
-  project     = "lab-devops-cloud-garcia173"
-  location_id = "us-central"
+resource "google_project" "my_project" {
+  name       = "DevOps Lab"
+  project_id = "devops-lab-garcia"
+  org_id     = "202105262139"
 }
 
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_project.project_id
+  location_id = "us-central"
+}
